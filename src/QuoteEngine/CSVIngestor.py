8621c72470a3c26f5b173331cfa35ee19e5a40ae
karinpietruska <karin.pietruska@gmail.com>
2021-07-25 16:48:17 +0200

    doc: ensure PEP-8 compliance

--- src/QuoteEngine/CSVIngestor.py
+++ src/QuoteEngine/CSVIngestor.py
@@ -1,5 +1,6 @@
+"""Module to import quotes in CSV files."""
 from typing import List
-import pandas as pd 
+import pandas as pd
 
 from .QuoteModel import QuoteModel
 from .IngestorInterface import IngestorInterface
@@ -11,7 +12,7 @@ class CSVIngestor(IngestorInterface):
     allowed_extensions = ["csv"]
 
     @classmethod
-    def parse(cls, path:str) -> List[QuoteModel]:
+    def parse(cls, path: str) -> List[QuoteModel]:
         """Parse quotes in CSV file."""
         ingestable = cls.can_ingest(path)
         if not ingestable:
@@ -19,16 +20,11 @@ class CSVIngestor(IngestorInterface):
 
         quotes_retrieved = list()
 
-        data_df = pd.read_csv(path,header='infer')
+        data_df = pd.read_csv(path, header='infer')
 
         for index, row in data_df.iterrows():
             body = str(row['body'])
             author = str(row['author'])
             quote = QuoteModel(body, author)
             quotes_retrieved.append(quote)
-    
-        return quotes_retrieved 
-
-
-
-            
+        return quotes_retrieved
