a503a5931d838aaae83bdb0a2980d65a0cbb18eb
karinpietruska <karin.pietruska@gmail.com>
2021-07-25 16:30:20 +0200

    feature: import csv with pandas (project requirement)

--- src/QuoteEngine/CSVIngestor.py
+++ src/QuoteEngine/CSVIngestor.py
@@ -1,12 +1,12 @@
 from typing import List
-import csv 
+import pandas as pd 
 
 from .QuoteModel import QuoteModel
 from .IngestorInterface import IngestorInterface
 
 
 class CSVIngestor(IngestorInterface):
-    """Parser for quotes in CSV files."""
+    """Parser to import quotes in CSV files."""
 
     allowed_extensions = ["csv"]
 
@@ -19,14 +19,14 @@ class CSVIngestor(IngestorInterface):
 
         quotes_retrieved = list()
 
-        with open(path, 'r') as infile:
-            reader = csv.reader(infile)
-            next(reader)
-            for row in reader:
-                body = str(row[0])
-                author = str(row[1])
-                quote = QuoteModel(body,author)
-                quotes_retrieved.append(quote)
+        data_df = pd.read_csv(path,header='infer')
+
+        for index, row in data_df.iterrows():
+            body = str(row['body'])
+            author = str(row['author'])
+            quote = QuoteModel(body, author)
+            quotes_retrieved.append(quote)
+    
         return quotes_retrieved 
 
 
