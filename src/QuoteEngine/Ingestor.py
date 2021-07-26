e531ff29412ae1d37fdcece4a8298128e662777d
karinpietruska <karin.pietruska@gmail.com>
2021-07-26 21:16:00 +0200

    doc: ensure PEP-8, PEP-257 compliance

--- src/QuoteEngine/Ingestor.py
+++ src/QuoteEngine/Ingestor.py
@@ -1,3 +1,4 @@
+"""Module for reading in quotes from files."""
 from typing import List
 from .IngestorInterface import IngestorInterface
 from .QuoteModel import QuoteModel
@@ -6,13 +7,16 @@ from .DocxIngestor import DocxIngestor
 from .PDFIngestor import PDFIngestor
 from .TextIngestor import TextIngestor
 
-class Ingestor(IngestorInterface):
 
-    allowed_parsers = [CSVIngestor, DocxIngestor,PDFIngestor, TextIngestor]
+class Ingestor(IngestorInterface):
+    """Class for reading in quotes from files."""
+
+    allowed_parsers = [CSVIngestor, DocxIngestor, PDFIngestor, TextIngestor]
 
     @classmethod
-    def parse(cls, path:str) -> List[QuoteModel]:
+    def parse(cls, path: str) -> List[QuoteModel]:
+        """Parse quotes from a given file."""
         for file_parser in cls.allowed_parsers:
             if file_parser.can_ingest(path):
-                qmodel_list  = file_parser.parse(path)
+                qmodel_list = file_parser.parse(path)
                 return qmodel_list
