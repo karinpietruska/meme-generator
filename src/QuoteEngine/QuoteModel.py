1a6139d2f60110eb28aa5ffa42dcf44ff814bcaa
karinpietruska <karin.pietruska@gmail.com>
2021-07-26 21:39:19 +0200

    doc: ensure PEP8, PEP-257 compliance

--- src/QuoteEngine/QuoteModel.py
+++ src/QuoteEngine/QuoteModel.py
@@ -1,7 +1,11 @@
+"""Module with class to encapsulate quote."""
+
+
 class QuoteModel():
     """A class used to encapsulate body and author of a quote."""
 
     def __init__(self, body, author):
+        """Initialize a QuoteModel instance."""
         self.body = body
         self.author = author
 
