10d318f5ebf700563ac443121ad3c93b18263a16
karinpietruska <karin.pietruska@gmail.com>
2021-07-26 21:21:18 +0200

    doc: ensure PEP-8, PEP-257 compliance

--- src/QuoteEngine/IngestorInterface.py
+++ src/QuoteEngine/IngestorInterface.py
@@ -1,21 +1,23 @@
+"""Abstract Interface for Ingestor classes."""
 from abc import ABC, abstractmethod
 from typing import List
 
 from .QuoteModel import QuoteModel
 
+
 class IngestorInterface(ABC):
     """Abstract class IngestorInterface."""
-
+    
     allowed_extensions = ["csv", "docx", "pdf", "txt"]
 
     @classmethod
-    def can_ingest(cls, path:str) -> bool:
+    def can_ingest(cls, path: str) -> bool:
         """Return true if file has readable format."""
         ext = path.split('.')[-1]
         return ext in cls.allowed_extensions
-    
+
     @classmethod
-    @abstractmethod     
-    def parse(cls, path:str) -> List[QuoteModel]:
+    @abstractmethod
+    def parse(cls, path: str) -> List[QuoteModel]:
         """Parse quotes and return list of quotes."""
         pass
